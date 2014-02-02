a36b0f61c7103a1da6d523cc95b82d282a721517
Vincent Boisard <vincent.boisard@dimelo.com>
2014-02-02 15:01:02 +0100

    fixed paths in schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+env 'APP_PATH', '/home/app/googly_eyes/'
+
 every 1.day, at: '4:00 am' do
-  command "find #{shared_path}/public/eyesoup/* -mtime +5 -exec rm {} \;"
+  command "find $APP_PATH/public/eyesoup/* -mtime +5 -exec rm {} \;"
 end
