6d43d8302a06234e42875baec4d092cf26fc8ba0
Vincent Boisard <vincent.boisard@dimelo.com>
2014-02-02 18:52:59 +0100

    feature: render image and home and provide permalink

--- public/javascripts/googlify.js
+++ public/javascripts/googlify.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
+function renderImage(url) {
+  var container = $("div.result");
+  container.find("input[name=permalink]").val(url);
+  container.find("img").attr('src', url);
+  container.removeClass('hidden');
+}
 
+$(document).ready(function() {
   $.getJSON("/styles", function(styles) {
     styles.forEach(function(style) {
       var opt = "<option name=" + style + ">" + style + "</option>";
@@ -7,4 +13,21 @@ $(document).ready(function() {
     });
   });
 
+  $('form#googlify').submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'HEAD',
+      url: event.target.action,
+      data: {
+        url: $(event.target).find("input[name=url]")[0].value,
+        style: $(event.target).find("select[name=style]")[0].value
+      },
+      success: function(data, status, xhr) {
+        renderImage(this.url);
+      },
+      error: function(data, status, xhr) {
+        alert("Could not add googly eyes on " + $(event.target).find("input[name=url]")[0].value);
+      }
+    });
+  });
 });
