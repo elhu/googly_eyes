7c06128cb7dcc0bd12062cdb894e97bcb287e704
Vincent Boisard <vincent.boisard@dimelo.com>
2014-02-02 15:04:30 +0100

    fix cron entry

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-env 'APP_PATH', '/home/app/googly_eyes/'
+env 'APP_PATH', '/home/app/googly-eyes/current'
 
 every 1.day, at: '4:00 am' do
-  command "find $APP_PATH/public/eyesoup/* -mtime +5 -exec rm {} \;"
+  command "find $APP_PATH/public/eyesoup/* -mtime +1 -exec rm {} \\;"
 end
